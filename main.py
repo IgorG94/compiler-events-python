0fd8b307a2c55a074aa1c9a079ae4cc4094f60e0
Igor Goldstein <igorgoldstein94@gmail.com>
2019-01-12 03:48:02 -0200

    adjust main.py for semantic analysis

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from analisadorLexicoSintatico.decompositor_caracteres import (
 from analisadorLexicoSintatico.analise_lexica import (
     AnalisadorLexico
 )
+from analisadorLexicoSintatico.analise_sintatica import (
+    AnalisadorSintatico
+)
 from notacaoWirth.analise_lexica_wirth import (
     AnalisadorLexicoWirth
 )
@@ -15,7 +18,8 @@ print("Voce deseja testar o decompositor de caracteres, ",
       "o analisador lexico ou o reconhecedor da Notacao de Wirth?")
 print("Digite 1 para decompositor de caracteres")
 print("Digite 2 para o analisador lexico")
-print("Digite 3 para o reconhecedor da Notacao de Wirth")
+print("Digite 3 para o analisador sintatico e semantico")
+print("Digite 4 para o reconhecedor da Notacao de Wirth")
 
 teste = input()
 
@@ -130,8 +134,61 @@ elif (teste == "2"):
         print("O arquivo ", nome_arq, " nao foi encontrado!")
         input("Pressione ENTER para sair")
 
-# Se for um teste do reconhecedor da Notação de Wirth
+# Se for um teste do analisador sintático e semântico
 elif (teste == "3"):
+    print("***** TESTE DO ANALISADOR SINTATICO E SEMANTICO *****")
+
+    nome_arq = input(
+        "Digite o nome do arquivo com o codigo fonte (ex.: teste1.txt): "
+    )
+    aux_imprime_lista_atomos = input(
+        "Imprimir no terminal os atomos (s/n)? "
+    )
+    aux_imprime_arquivo = input(
+        "Imprimir em um arquivo a lista de atomos (s/n)? "
+    )
+
+    imprime_lista_atomos = False
+    imprime_arquivo = False
+
+    if (aux_imprime_lista_atomos == "s"):
+        imprime_lista_atomos = True
+
+    if (aux_imprime_arquivo == "s"):
+        imprime_arquivo = True
+
+    # Leitura do arquivo
+    try:
+        arq = open(nome_arq, "r")
+        arq.close()
+
+        decompositor = DecompositorCaracteres()
+        decompositor.simulacao(nome_arq, False, False, False, False, False)
+
+        lista_char = decompositor.lista_caracteres
+
+        decompositor.lista_eventos = []
+        lexico = AnalisadorLexico()
+        lexico.simulacao(lista_char, False, False, False)
+
+        lista_atomos = lexico.lista_atomos
+
+        lexico.lista_eventos = []
+        sintatico = AnalisadorSintatico()
+        sintatico.simulacao(
+            lista_atomos,
+            imprime_lista_atomos,
+            imprime_arquivo
+        )
+
+        sair = input("Pressione ENTER para sair")
+
+    except FileNotFoundError:
+        print("O arquivo ", nome_arq, " nao foi encontrado!")
+        input("Pressione ENTER para sair")
+
+# Se for um teste do reconhecedor da Notação de Wirth
+elif (teste == "4"):
     print("***** TESTE DO RECONHECEDOR DA NOTACAO DE WIRTH *****")
     print("Qual parte do reconhecedor voce deseja testar?")
     print("Digite 1 para o decompositor de caracteres")
